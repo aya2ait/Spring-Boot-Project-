e2f8f980ece9fd2cb27bed1de3d9b3b34669c34f
Oumaima Boughdad <oumaima.boughdad@etu.uae.ac.ma>
2024-11-09 15:28:14 +0100

    oumaima commitmenet

--- src/main/java/com/mini/prj/entities/Retrait.java
+++ src/main/java/com/mini/prj/entities/Retrait.java
@@ -15,8 +15,22 @@ public class Retrait extends Operation{
     }
 
 
-    public void Retrai(int id){
+    public String effectuerRetrait(Compte compte ,double montantRetrait){
+        if(compte.getSolde() >= montantRetrait){
+            compte.setSolde(compte.getSolde() - montantRetrait);
+            this.setCompte(compte);
+            this.setMontant(montantRetrait);
+            this.setDateOperation(new Date());
+
+
+            return "Retrait effectué avec succès. Nouveau solde: " + compte.getSolde();
+        }
+        else {
+            // Retourner un message d'erreur si le solde est insuffisant
+            return "Erreur: Solde insuffisant pour effectuer le retrait.";
+        }
 
     }
+
 }
 
--- src/main/java/com/mini/prj/services/OperationRestService.java
+++ src/main/java/com/mini/prj/services/OperationRestService.java
@@ -2,12 +2,15 @@ package com.mini.prj.services;
 
 
 import com.mini.prj.entities.Client;
+import com.mini.prj.entities.Compte;
 import com.mini.prj.entities.Operation;
+import com.mini.prj.entities.Retrait;
 import com.mini.prj.metier.OperationMetier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -39,4 +42,22 @@ public class OperationRestService {
     public Operation updateOperation(@PathVariable("id") Long id, @RequestParam("montant") Double montant) {
         return operationMetier.updateOperation(id,montant);
     }
+
+    //la methode retrait
+
+    @PostMapping("/retirer")
+    public String effectuerRetrait(@RequestBody String codeCompte, @RequestParam Double montant) {
+        // Call the service method to perform the withdrawal
+        return operationMetier.effeffectuerRetrait(codeCompte, montant);
+    }
+
+    @PostMapping("/verser")
+    public String effectuerVersement(@RequestBody Map<String, Object> requestBody) {
+        String codeCompte = (String) requestBody.get("codeCompte");
+        Double montant = (Double) requestBody.get("montant");
+
+        // Call the service method
+        return operationMetier.effeffectuerVersement(codeCompte, montant);
+    }
+
 }
